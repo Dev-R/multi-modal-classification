48994fefd43c1ff237c4f655b195e4a459f595cb
Dev-R <rabimhd@icloud.com>
2023-02-09 00:57:59 +0800

    Update contant

--- helpers/constants.py
+++ helpers/constants.py
@@ -11,8 +11,10 @@ class ActionRecognition:
     CLASSES_LIST = os.listdir(DATASET_DIRECTORY)  # Return all classes name in UCF50
     MODEL = "modules/video_classification_CNN/Model___Date_Time_2023_01_23__22_37_31___Loss_0.5416110157966614___Accuracy_0.9529687762260437.h5"
 
-    # Video classes that will trigger a red flag
-    ANOMALY_CLASSES_NAME = ["Explosion"]
+    ANOMALY_CLASSES_NAME = ["Normal"]  # Video classes that will trigger a red flag
+    ANOMALY_CLASS_CONFIDENCE = (
+        0.8  # Only trigger alarm when class confidence at this level: Should be 0 < ANOMALY_CLASS_CONFIDENCE <= 1 
+    )
 
     TOP_CONFIDENCE_CLASS_TEXT_COLOR = webcolors.name_to_rgb("green")
 
@@ -20,7 +22,7 @@ class ActionRecognition:
 
     MID_CONFIDENCE_CLASS_TEXT_COLOR = webcolors.name_to_rgb("yellow")
 
-    LOW_CONFIDENCE_CLASS_TEXT_COLOR = webcolors.name_to_rgb("red")
+    LOW_CONFIDENCE_CLASS_TEXT_COLOR = webcolors.name_to_rgb("white")
 
 
 class AudioRecognition:
@@ -66,6 +68,32 @@ class ObjectDetection:
 class Config:
 
     # Directory to save image path
-
     MEDIA_PATH = "outputs"
+
+    # Notification daemon processes config: Time between messages: https://www.ibm.com/docs/en/aix/7.1?topic=processes-
     NOTIFICATION_CONFIG = {"notification_type": "sms", "notification_interval": 60 * 2}
+
+    # SMS and Notifications
+    TO_PHONE_NUMBER = ""
+    FROM_PHONE_NUMBER = ""
+
+    # Twilio Config
+    # Read more at http://twil.io/secure
+    ACCOUNT_SID = ""
+    AUTH_TOKEN = ""
+    MESSAGE_TEMPLATE = "simple_message_template_b"
+    MESSAGE_SERVICE_ID = ""
+
+    # Azure BLOB Config
+    CONNECT_STR = ""  # Azure Connection String
+    IMAGE_CONTAINER_NAME = "ima-uploads"
+    VIDEO_CONTAINER_NAME = "video-uploads"
+    AZURE_STORGE_ACCOUNT = "multimodalmedia"
+
+
+
+    # Steaming type
+    STREAMING_CAM_MODE = 'Local'
+    STREAMING_LINK = 'rtsp://AdminDemo:adminDemo@192.168.1.17/stream1'
+    STREAMING_CONFIG = {'Local': 0, 'IP': STREAMING_LINK} 
+    STREAMING_MODE = STREAMING_CONFIG[STREAMING_CAM_MODE]
